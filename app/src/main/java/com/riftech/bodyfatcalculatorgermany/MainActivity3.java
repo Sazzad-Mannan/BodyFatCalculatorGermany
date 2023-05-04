67e7c8bf4262882076ddeeb933036de1923aef12
Sazzad-Mannan <sazzadmannan16@gmail.com>
2023-05-05 03:09:00 +0600

    commit

---
+++ app/src/main/java/com/riftech/bodyfatcalculatorgermany/MainActivity3.java
@@ -0,0 +1,24 @@
+package com.riftech.bodyfatcalculatorgermany;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+
+
+
+    }
+}
